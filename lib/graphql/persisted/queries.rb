1820fcdca87a0012c113cd6dbbd411dbdac07406
Damon Aw <daemonsy@gmail.com>
2018-09-13 21:03:52 -0400

    Initial commit

---
+++ lib/graphql/persisted/queries.rb
@@ -0,0 +1,9 @@
+require "graphql/persisted/queries/version"
+
+module Graphql
+  module Persisted
+    module Queries
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/graphql/persisted/queries/version.rb
@@ -0,0 +1,7 @@
+module Graphql
+  module Persisted
+    module Queries
+      VERSION = "0.1.0"
+    end
+  end
+end
